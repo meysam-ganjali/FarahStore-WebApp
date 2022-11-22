5091159699082e3901f581f6b11e00aee64b1fc8
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-11-22 06:48:02 -0800

    Index Page For Category

---
+++ FarahStoreWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using FarahStoreApplication.UnitOfWorkPattern;
+using FarahStoreWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarahStoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+
+        public CategoryController(IUnitOfWork unitOfWork, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            _unitOfWork = unitOfWork;
+            _environment = environment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var res = await _unitOfWork.Category.GetAll(includeProperties: "ParentCategory,SubCategories");
+            return View(res);
+        }
+    }
+}
