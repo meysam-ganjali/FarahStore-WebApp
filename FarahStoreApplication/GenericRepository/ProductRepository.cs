b37a23c4210ac100b4c063aac699b84ddb3a2814
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-11-23 12:05:38 -0800

    Create Index page for Product

--- FarahStoreApplication/GenericRepository/ProductRepository.cs
+++ FarahStoreApplication/GenericRepository/ProductRepository.cs
@@ -15,6 +15,28 @@ public class ProductRepository:Repository<Product>, IProductRepository
     }
     public async Task<ResultDto> Update(Product product)
     {
-        throw new NotImplementedException();
+        var productFromDb = await _db.Products.FindAsync(product.Id);
+        if (productFromDb == null)
+        {
+            return new ResultDto
+            {
+                Status = false,
+                Message = "محصولی با این مشخصات در سیستم وجود ندارد"
+            };
+        }
+        productFromDb.Name=product.Name;
+        productFromDb.ChaildCategoryCategoryId=product.ChaildCategoryCategoryId;
+        productFromDb.DiscountLable=product.DiscountLable;
+        productFromDb.Price = product.Price;
+        productFromDb.FullDescription=product.FullDescription;
+        productFromDb.NumberOfEntries=product.NumberOfEntries;
+        productFromDb.OldPrice=product.OldPrice;
+        productFromDb.ShortDescription=product.ShortDescription;
+        productFromDb.Slug=product.Slug;
+        return new ResultDto
+        {
+            Status = true,
+            Message = "محصول مورد نظر بروز شد"
+        };
     }
 }
