50643ab146b9407125ef334d257f46e541e10bac
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-11-24 05:42:16 -0800

    Handle Create Product

--- FarahStoreWeb/Areas/Admin/Controllers/ProductController.cs
+++ FarahStoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,13 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FarahStoreApplication.UnitOfWorkPattern;
+using FarahStoreModel.Models;
+using FarahStoreWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace FarahStoreWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        #region Ctor
+
+
+        private readonly IUnitOfWork _unitOfWork;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+
+        public ProductController(IUnitOfWork unitOfWork, IHostingEnvironment environment)
         {
+            _unitOfWork = unitOfWork;
+            _environment = environment;
+        }
+
+        #endregion
+
+        #region Product Action Method
+
+        public async Task<IActionResult> Index(string? searchKey)
+        {
+
+            var products = await _unitOfWork.Product.GetAll(includeProperties: "ChaildCategory");
+
+
+            if (!string.IsNullOrWhiteSpace(searchKey))
+            {
+                products = await _unitOfWork.Product.GetAll(
+                   includeProperties: "ChaildCategory",
+                   filter: u => u.Name.Contains(searchKey));
+            }
+            return View(products);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.Category = new SelectList(await _unitOfWork.ChaildCategory.GetAll(includeProperties: "Category"),
+                "Id", "Name");
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            var res = await _unitOfWork.Product.Add(product);
+
+            if (res.Status)
+            {
+                TempData["Message"] = res.Message;
+                TempData["MessageType"] = "Success";
+                await _unitOfWork.Save();
+                return Redirect("/Admin/Product/Index");
+                
+            }
+
+            TempData["Message"] = res.Message;
+            TempData["MessageType"] = "Error";
+            return Redirect("/Admin/Product/Index");
+        }
+        #endregion
+
     }
 }
